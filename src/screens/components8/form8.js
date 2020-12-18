3de6c580c4974158fa136173a9301a075472a3dc
Kushaan Gowda <kushaangowda@yahoo.com>
2020-12-18 21:56:51 +0530

    finished Screen 8

--- src/screens/components8/form8.js
+++ src/screens/components8/form8.js
@@ -1,9 +1,33 @@
 import React from "react";
+import wharr from "../../assets/arrow-down-sign-to-navigate-8.svg";
 
 function Form8() {
 	return (
 		<div className="Form8">
 			<h1>Send Us message</h1>
+			<form>
+				<p>Your Email*</p>
+				<input type="text" />
+				<p>Your Name*</p>
+				<input type="text" />
+				<p>Phone Number</p>
+				<input type="text" />
+				<p>Subject</p>
+				<input type="text" />
+				<p>Your Message</p>
+				<input type="text" />
+				<br />
+				<br />
+				<input type="checkbox" id="c" value="a"></input>
+				<span>By sending this message, you confirm that you have read and agreed to our privacy-policy.</span>
+				<br />
+				<br />
+				<button>
+					Get Started&nbsp;&nbsp;
+					<img src={wharr} className="wharr" />
+					<img src={wharr} className="wharr" />
+				</button>
+			</form>
 		</div>
 	);
 }
