34d8d81ba9eddd906e037ecce0ec2081bce62f26
Kushaan Gowda <kushaangowda@yahoo.com>
2020-12-18 13:11:42 +0530

    finished Screen 4

--- src/screens/Screen4.js
+++ src/screens/Screen4.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Card4 from "./components4/card4";
+import t41 from "../assets/Group 35700.svg";
 
 function Screen4() {
 	return (
 		<div className="Screen4">
-			<h1>Hi</h1>
+			<p className="heading4">Workflow</p>
+			<p className="desc4">
+				Lorem ipsum dolor sit amet, conse ctetur adipi scing elit. Aliq uam ex odio, is the turpis accu msan congue euisque blandit dui Pelle ntesque
+				habitant.
+			</p>
+			<Card4 />
+			<img src={t41} className="t41" />
 		</div>
 	);
 }
---
+++ src/screens/components4/card4.js
@@ -0,0 +1,47 @@
+import React from "react";
+import line from "../../assets/Rectangle 1326.svg";
+
+function Card4() {
+	return (
+		<div className="Card4">
+			<div className="card41">
+				<button className="btn4">STEP 1</button>
+				<p class="heading41">Send your brief</p>
+				<img src={line} className="line" />
+				<p className="desc41">
+					Lorem ipsum dolor sit amet, conse ctetur adipi scing elit. Aliq uam ex odio, is the turpis accu msan congue euisque blandit dui Pelle
+					ntesque habitant.
+					<br />
+					<br />
+					Lorem ipsum dolor sit amet, conse ctetur adipi scing elit. Aliq uam ex odio, is the turpis accu
+				</p>
+			</div>
+			<div className="card41">
+				<button className="btn4">STEP 2</button>
+				<p class="heading41">Wait for delivery</p>
+				<img src={line} className="line" />
+				<p className="desc41">
+					Lorem ipsum dolor sit amet, conse ctetur adipi scing elit. Aliq uam ex odio, is the turpis accu msan congue euisque blandit dui Pelle
+					ntesque habitant.
+					<br />
+					<br />
+					Lorem ipsum dolor sit amet, conse ctetur adipi scing elit. Aliq uam ex odio, is the turpis accu
+				</p>
+			</div>
+			<div className="card41">
+				<button className="btn4">STEP 3</button>
+				<p class="heading41">Get your files!</p>
+				<img src={line} className="line" />
+				<p className="desc41">
+					Lorem ipsum dolor sit amet, conse ctetur adipi scing elit. Aliq uam ex odio, is the turpis accu msan congue euisque blandit dui Pelle
+					ntesque habitant.
+					<br />
+					<br />
+					Lorem ipsum dolor sit amet, conse ctetur adipi scing elit. Aliq uam ex odio, is the turpis accu
+				</p>
+			</div>
+		</div>
+	);
+}
+
+export default Card4;
