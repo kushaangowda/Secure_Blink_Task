b99d5f83fb5dd279a2fba993df7c848f0285cbdd
Kushaan Gowda <kushaangowda@yahoo.com>
2020-12-19 11:58:15 +0530

    fixed Screen 3

--- src/screens/Screen3.js
+++ src/screens/Screen3.js
@@ -5,8 +5,9 @@ import Card3 from "./components3/card3";
 function Screen3() {
 	return (
 		<div className="Screen3" id="services">
-			<div className="part1"></div>
-			<Text3 />
+			<div className="part1">
+				<Text3 />
+			</div>
 			<Card3 />
 		</div>
 	);
